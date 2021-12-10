d4523daac1036377de6d038501a7b16343c245b2
KarymeSilva <karyme.silva.279@gmail.com>
2021-12-10 01:34:18 -0600

    Second Commit

--- workbox-config.js
+++ workbox-config.js
@@ -3,7 +3,7 @@ module.exports = {
 	globPatterns: [
 		'**/*.{css,eot,svg,ttf,woff,png,jpg,PNG,js,scss,html,json}'
 	],
-	swDest: 'service-worker.js',
+	swDest: 'sw.js',
 	ignoreURLParametersMatching: [
 		/^utm_/,
 		/^fbclid$/
